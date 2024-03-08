090e2385752c59271f679136e7893cb4640d498d
Mikhail Mikhailov <mikhailoff@inbox.ru>
2024-03-08 16:31:36 +0300

    ADD: lit

--- dropbox.f
+++ dropbox.f
@@ -4,3 +4,4 @@ rails create browser -l web-kit
 Serafima The Beyuty at 7-th college FF Forward
 Работаю на отрицательном ампераже (Анрэ Луи Ампер открыл силу тока и измерил) A
 94.25.239.79 remote D-Link forward safe-thunder Server Apache-Irbis
+SFNetwork.pod - pod - библиотека(либа)на устройствах Apple. >> pod install SFNetwork. \n для работы девайса/гаджета в Интернет или ограниченной сети или безграничной сети с адресацией IPv4
